fad36479c8ebef8b27bf651122410c5b05ef1796
Archana <archana10dpatil@gmail.com>
2014-12-08 14:17:21 +0530

    add user login and sign_in with github

---
+++ app/controllers/authentications_controller.rb
@@ -0,0 +1,14 @@
+class AuthenticationsController < ApplicationController
+	def create
+		#binding.pry
+	auth = request.env["omniauth.auth"]
+	user = User.from_omniauth(auth)
+	sign_in(user)
+  #current_user.authentications.find_or_create_by_provider_and_uid(auth['provider'], auth['uid'])
+  flash[:notice] = "Authentication successful."
+  redirect_to root_url
+	end
+
+	def destroy
+	end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,42 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  has_many :authentications
+
+  def self.from_omniauth(auth)
+    #binding.pry
+    
+    password = Devise.friendly_token.first(8)
+   # u= User.create_with(password: password, password_confirmation: password,name: auth["extra"]["raw_info"]["login"])
+     #user = u.find_or_create_by(email: auth["info"]["email"])
+     user = User.find_by(email: auth["info"]["email"])
+     unless user.present?
+       user = User.create(email: auth["info"]["email"],password: password, password_confirmation: password,name: auth["extra"]["raw_info"]["login"])
+      end 
+     #binding.pry
+     authentication = user.authentications.find_or_create_by(provider: auth['provider'],uid: auth['uid'])
+     #binding.pry
+     if authentication.present?
+        user = authentication.user
+      end
+     
+     user.authentications.find_or_create_by(provider: auth['provider'], uid: auth['uid'])
+     user
+=begin
+      where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.name = auth.info.name
+      user.email = auth.info.email
+      user.password = auth.password
+      #user.password_confirmation = auth.info.password_confirmation
+      user.oauth_token = auth.credentials.token
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.save!
+    end
+=end
+
+  end
+end
