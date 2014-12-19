3bd70a996acbf387b090fad835e6e930548262b7
Archana <archana10dpatil@gmail.com>
2014-12-19 17:38:38 +0530

    remove gchart

--- config/application.rb
+++ config/application.rb
@@ -2,10 +2,9 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-# Rrequire 'gchart'equire the gems listed in Gemfile, including any gems
+
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-require 'gchart'
 module GithubManagement
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
